74bf77d9e9d6a3058e95a32e39c98ed0b270f70b
Nikhil Garg <g1.nikhil@gmail.com>
2017-12-07 01:59:53 -0500

    Worked on CustomTurney more

--- CustomTurneyYelp.py
+++ CustomTurneyYelp.py
@@ -2,105 +2,53 @@ import math
 from ParseYelpData import stream_pos_reviews, stream_neg_reviews
 from tqdm import tqdm
 import re
-
+import string
 import nltk
 
-pos_lex_file = "positive-words.txt"
-neg_lex_file = "negative-words.txt"
 
-pos_lexicon = []
-neg_lexicon = []
 
-def stream_pos_lexicon(pos_lex_file):
-    for i, line in enumerate(open(pos_lex_file)):
+def stream_lexicon(lex_file, lexicon):
+    file = open(lex_file)
+    for i, line in enumerate(file):
         if i >= 35 and line != "" and line != "\n":
-            pos_lexicon += line
-    pos_lex_file.close()
-
-def stream_neg_lexicon(neg_lex_file):
-    for i, line in enumerate(open(neg_lex_file)):
-        if i >= 35 and line != "" and line != "\n":
-            neg_lexicon += line
-    neg_lex_file.close()
-
-def custom_turney():    
-    review_json = "../Yelp_dataset/review.json"
-    business_json = "../Yelp_dataset/business.json"
+            lexicon.append(line)
+    file.close()
     
-    # The training set for positive reviews
-    pos_training_set = []
-    # The training set for negative reviews
-    neg_training_set = []
-
-    # Initialize positive training set
-    pbar = tqdm(total = 100)
+    
+    
+def initialize_sample_set(sample_set, stream_function, jsons, start, finish):
+    pbar = tqdm(total = (finish-start))
     count = 0
-    for pos_review in stream_pos_reviews(review_json, business_json):
-        if count < 100:
-            pos_training_set.append(pos_review)
+    for review in stream_function(jsons[0], jsons[1]):
+        if count >= start and count < finish:
+            sample_set.append(review)
             pbar.update()
+            count += 1
+        elif count < start:
             count += 1
         else:
             pbar.close()
-            break
+            break                          
 
-    # Initialize negative training set
-    pbar = tqdm(total = 100)
-    count = 0
-    for neg_review in stream_neg_reviews(review_json, business_json):
-        if count < 100:
-            neg_training_set.append(neg_review)
-            pbar.update()
-            count += 1
-        else:
-            pbar.close()
-            break
-
-    # Pseudocount:
-    alpha = 0.0001
-    
-    # Counts of positive and negative seeds, respectively
-    num_pos_seeds = 0
-    num_neg_seeds = 0
-    
-    # Set of all words
-    nouns = set()
-    
-    # Lists of all actual positive nouns and actual negative nouns
-    actual_pos_nouns = set()
-    actual_neg_nouns = set()
-    
-    # Counts of times a word appears in same sentence as positive or negative reviews, respectively
-    num_word_near_pos_dict = dict()
-    num_word_near_neg_dict = dict()
-    
-    # Dict of noun polarity scores
-    noun_polarity_scores = dict()
-    
-    # Noun counts
-    noun_counts = dict()
-
-    # List of positive and negative seeds
-    stream_pos_lexicon(pos_lex_file)
-    stream_neg_lexicon(neg_lex_file)
-    
-    pos_seeds = pos_lexicon
-    neg_seeds = neg_lexicon
-
-    # Parse all positive reviews, one positive review at a time
-    pbar2 = tqdm(total = len(pos_training_set))
-    for pos_review in pos_training_set:
-        
-        # Split positive review into sentences separated by spaces
-        pos_review_sentences = nltk.sent_tokenize(pos_review)
-        
-        
-        for sentence in pos_review_sentences:
-            sentence_without_punctuation = sentence.translate(None, string.punctuation)
             
-            # Used for determining if review is positive
-            num_pos_seeds_in_sentence = 0
-            num_neg_seeds in sentence = 0
+            
+def parse_reviews(training_set, total_seeds, seeds, num_seeds, nouns, review_noun_dict, actual_polarized_nouns, num_noun_near_seed_dict, noun_counts):
+    # Parse all reviews in training set, one review at a time
+    pbar2 = tqdm(total = len(training_set))
+    for review in training_set:
+        
+        review_noun_dict[review] = []
+        
+        # Split review into sentences separated by spaces
+        review_sentences = nltk.sent_tokenize(review)
+        
+        # Parse sentence
+        for sentence in review_sentences:
+            # Removes punctuation from sentence
+            sentence_without_punctuation = "".join(l for l in sentence if l not in string.punctuation)
+            
+            # Used for determining if review is positive or negative
+            num_seeds_in_sentence = 0
             
             # Split sentence into words and parts of speech tags
             words_in_sentence = nltk.pos_tag(sentence_without_punctuation)
@@ -116,117 +64,154 @@ def custom_turney():
                         noun_counts[word] = 1
                     else:
                         noun_counts[word] += 1
-                    nouns_in_sentence += word
-                # If positive seed is found, mark sentence as positive and update count of positive sentences
-                if word in pos_seeds:
-                    num_pos_seeds += 1
-                    num_pos_seeds_in_sentence += 1
+                    nouns_in_sentence.append(word)
+                # If seed is found, update number of (either positive or negative) seeds, both in sentence and in total
+                if word in seeds:
+                    num_seeds += 1
+                    num_seeds_in_sentence += 1
 
             # Parse nouns in sentence
             for noun in nouns_in_sentence:
-                # Check to make sure word isn't a seed
-                if noun not in pos_seeds and word not in neg_seeds:
-                    # Update count of times word appears near positive seed
+                # Check to make sure noun isn't a seed
+                if noun not in total_seeds:
+                    # Append noun to review-noun dictionary
+                    review_noun_dict[review].append(noun)
+                    # Update count of times noun appears near positive seed
                     if noun not in nouns:
                         nouns.add(noun)
-                        actual_pos_nouns.add(noun)
-                    num_noun_near_pos_dict[word] = 0
-                    num_noun_near_pos_dict[word] += num_pos_seeds_in_sentence
+                        actual_polarized_nouns.add(noun)
+                    if noun not in num_noun_near_seed_dict.keys():
+                        num_noun_near_seed_dict[noun] = 0
+                    num_noun_near_seed_dict[noun] += num_seeds_in_sentence
         
         pbar2.update()
     pbar2.close()
-    
-    # Parse all negative reviews, one negative review at a time
-    pbar3 = tqdm(total = len(neg_training_set))
-    for neg_review in neg_training_set:
-        
-        # Split negative review into sentences separated by spaces
-        neg_review_sentences = nltk.sent_tokenize(neg_review)
-        
-        for sentence in neg_review_sentences:
-            sentence_without_punctuation = sentence.translate(None, string.punctuation)
-            
-            # Used for determining if review is negative
-            num_neg_seeds_in_sentence = 0
-            
-            # Split sentence into words and parts of speech tags
-            words_in_sentence = nltk.pos_tag(sentence_without_punctuation)
-            
-            # List of nouns in sentence
-            nouns_in_sentence = []
-            
-            # Parse sentence for nouns and negativity measure
-            for (word, part_of_speech_tag) in words_in_sentence:
-                # Update noun count (both in sentence and in total)
-                if part_of_speech_tag in ['NN', 'NNS']:
-                    if word not in nouns:
-                        noun_counts[word] = 1
-                    else:
-                        noun_counts[word] += 1
-                    nouns_in_sentence += word
-                # If negative seed is found, mark sentence as negative and update count of negative sentences
-                if word in neg_seeds:
-                    num_neg_seeds += 1
-                    num_neg_seeds_in_sentence += 1
 
-            # Parse nouns in sentence
-            for noun in nouns_in_sentence:
-                # Check to make sure word isn't a seed
-                if noun not in pos_seeds and word not in neg_seeds:
-                    # Update count of times word appears near negative seed
-                    if noun not in nouns:
-                        nouns.add(noun)
-                        actual_neg_nouns.add(noun)
-                    num_noun_near_neg_dict[word] = 0
-                    num_noun_near_neg_dict[word] += num_neg_seeds_in_sentence
-        
-        pbar3.update()
-    pbar3.close()
     
-    # Calculate word polarity scores
+
+def custom_turney(jsons, lex_files):    
+    
+    pos_training_set = [] # The training set for positive reviews
+    neg_training_set = [] # The training set for negative reviews
+
+    print "Initializing training sets"
+    initialize_sample_set(pos_training_set, stream_pos_reviews, jsons, 0, 100) # Initialize positive training set
+    initialize_sample_set(neg_training_set, stream_neg_reviews, jsons, 0, 100) # Initialize negative training set
+
+    # Pseudocount:
+    alpha = 0.0001
+    
+    # Counts of positive and negative seeds, respectively
+    num_pos_seeds = 0
+    num_neg_seeds = 0
+    
+    # Set of all words
+    nouns = set()
+    
+    # Dict of reviews and the nouns in each of them
+    review_noun_dict = dict()
+    
+    # Lists of all actual positive nouns and actual negative nouns
+    actual_pos_nouns = set()
+    actual_neg_nouns = set()
+    
+    # Counts of times a word appears in same sentence as positive or negative reviews, respectively
+    num_noun_near_pos_dict = dict()
+    num_noun_near_neg_dict = dict()
+    
+    # Dict of noun polarity scores
+    noun_polarity_scores = dict()
+    
+    # Noun counts
+    noun_counts = dict()
+
+    # List of positive and negative seeds
+    pos_seeds = []
+    neg_seeds = []
+    print "Streaming lexicons"
+    stream_lexicon(lex_files[0], pos_seeds)
+    stream_lexicon(lex_files[1], neg_seeds)
+    
+    total_seeds = pos_seeds + neg_seeds
+
+    print "Parsing training sets"
+    # Parse positive training set
+    parse_reviews(pos_training_set, total_seeds, pos_seeds, num_pos_seeds, nouns, review_noun_dict, actual_pos_nouns, num_noun_near_pos_dict, noun_counts)
+    # Parse negative training set
+    parse_reviews(neg_training_set, total_seeds, neg_seeds, num_neg_seeds, nouns, review_noun_dict, actual_neg_nouns, num_noun_near_neg_dict, noun_counts)
+    
+    print "Calculating polarity scores"
+    # Calculate noun polarity scores
     for noun in nouns:
-        if word_counts[noun] >= 500:
-            word_polarity_scores[noun] = math.log((num_noun_near_pos_dict[noun] + alpha)*num_neg_seeds*1.0/((num_noun_near_neg_dict[noun] + alpha)*num_pos_seeds*1.0),2)
+        if noun not in num_noun_near_pos_dict.keys():
+            num_noun_near_pos_dict[noun] = 0.0
+        if noun not in num_noun_near_neg_dict.keys():
+            num_noun_near_neg_dict[noun] = 0.0
+        numerator = (num_noun_near_pos_dict[noun] + alpha)*num_neg_seeds*1.0
+        denominator = (num_noun_near_neg_dict[noun] + alpha)*num_pos_seeds*1.0
+        print "Numerator: ", numerator
+        print "Denominator: ", denominator
+        print "Num pos seeds: ", num_pos_seeds
+        print "Num neg seeds: ", num_neg_seeds
+        print "Alpha: ", alpha
+        print "Num nouns near pos: ", num_noun_near_pos_dict[noun]
+        print "Num nouns near neg: ", num_noun_near_neg_dict[noun]
+        ratio = numerator/denominator
+        noun_polarity_scores[noun] = math.log(ratio,2)
     
-    return word_polarity_scores
+    return review_noun_dict, noun_polarity_scores, actual_pos_nouns, actual_neg_nouns
+
+
+
+# Returns number of reviews turney categorized correctly in the test set
+def eval_turney(test_set, review_noun_dict, noun_polarity_scores):
+    correct = 0
+    pbar3 = tqdm(total = len(test_set))
+    for review in test_set:
+        review_polarity_score = 0
+        for noun in review_noun_dict[review]:
+            review_polarity_score += noun_polarity_scores[noun]
+        if review_polarity_score > 0:
+            correct += 1
+    return correct
 
 
 
 
+##############################################
 
-word_polarity_scores = custom_turney()
 
+
+
+# Run custom turney
+
+review_json = "../Yelp_dataset/review.json"
+business_json = "../Yelp_dataset/business.json"
+jsons = (review_json, business_json)
+
+pos_lex_file = "positive-words.txt"
+neg_lex_file = "negative-words.txt"
+lex_files = (pos_lex_file, neg_lex_file)
+
+print "Running Custom Turney"
+review_noun_dict, noun_polarity_scores, actual_pos_nouns, actual_neg_nouns = custom_turney(jsons, lex_files)
+
+
+print "Initializing test sets"
 # Initialize positive test set
 pos_test_set = []
-pbar4 = tqdm(total = 200)
-count = 0
-for pos_review in stream_pos_reviews(review_json, business_json):
-    if count >= 100 and count < 200:
-        pos_test_set.append(pos_review)
-        pbar4.update()
-        count += 1
-    elif count < 100:
-        pbar4.update()
-        count += 1
-    else:
-        pbar4.close()
-        break
+initialize_sample_set(pos_test_set, stream_pos_reviews, jsons, 100, 150)
 
 # Initialize negative test set
 neg_test_set = []
-pbar4 = tqdm(total = 200)
-count = 0
-for neg_review in stream_neg_reviews(review_json, business_json):
-    if count >= 100 and count < 200:
-        neg_test_set.append(neg_review)
-        pbar4.update()
-        count += 1
-    elif count < 100:
-        pbar4.update()
-        count += 1
-    else:
-        pbar4.close()
-        break
+initialize_sample_set(neg_test_set, stream_neg_reviews, jsons, 100, 150)
 
-for pos_review in pos_test_set:
-    
+
+# Turney's method classifies reviews as positive or negative by adding the word polarity scores in the review
+
+# Evaluate custom turney
+
+# Evaluate on positive test set
+pos_correct = eval_turney(pos_test_set, review_noun_dict, noun_polarity_scores)
+neg_correct = eval_turney(neg_test_set, review_noun_dict, noun_polarity_scores)
+print "Accuracy of Custom Turney: ", 100.0*(pos_correct + neg_correct)/(len(pos_test_set) + len(neg_test_set))
